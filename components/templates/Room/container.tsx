61bb117cbc9c9fd9aaccd2634f69b27b60426458
dadada <nuhhunnuhhun@gmail.com>
2023-01-09 06:05:18 +0900

    wip: room page (enable change position)

--- components/templates/Room/container.tsx
+++ components/templates/Room/container.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
 import { Room } from '../../../resources/Room/type'
 import Presentation from './presentation'
 
@@ -9,46 +9,64 @@ const _user1 = {
   type: 'user' as const,
 }
 
+const roomData: Room = {
+  id: 0,
+  members: [_user1],
+  postIts: [
+    {
+      id: 0,
+      text: 'test',
+      themeId: 0,
+      position: {
+        x: 0,
+        y: 0,
+        z: 1,
+      },
+      size: {
+        width: 200,
+        height: 200,
+      },
+      user: _user1,
+      lastUpdate: '2023-01-01T12:00',
+    },
+    {
+      id: 1,
+      text: 'test2',
+      themeId: 1,
+      position: {
+        x: 100,
+        y: 100,
+        z: 0,
+      },
+      size: {
+        width: 200,
+        height: 200,
+      },
+      user: _user1,
+      lastUpdate: '2023-01-01T12:00',
+    },
+  ],
+}
+
 const Container: FC = () => {
-  const roomData: Room = {
-    id: 0,
-    members: [_user1],
-    postIts: [
-      {
-        id: 0,
-        text: 'test',
-        themeId: 0,
+  const [dummyUpdatedData, setDummyUpdatedData] = useState(roomData)
+  const onPostItMove = (next: { id: number; position: { x: number; y: number } }) => {
+    const newPostIts = dummyUpdatedData.postIts.map((prev) => {
+      if (prev.id !== next.id) return prev
+      return {
+        ...prev,
         position: {
-          x: 0,
-          y: 0,
-          z: 1,
+          ...next.position,
+          z: prev.position.z,
         },
-        size: {
-          width: 200,
-          height: 200,
-        },
-        user: _user1,
-        lastUpdate: '2023-01-01T12:00',
-      },
-      {
-        id: 1,
-        text: 'test2',
-        themeId: 1,
-        position: {
-          x: 100,
-          y: 100,
-          z: 0,
-        },
-        size: {
-          width: 200,
-          height: 200,
-        },
-        user: _user1,
-        lastUpdate: '2023-01-01T12:00',
-      },
-    ],
+      }
+    })
+    setDummyUpdatedData({
+      ...dummyUpdatedData,
+      postIts: newPostIts,
+    })
   }
-  return <Presentation {...roomData} />
+  return <Presentation {...dummyUpdatedData} onPostItMove={onPostItMove} />
 }
 
 export default Container
--- components/templates/Room/presentation.tsx
+++ components/templates/Room/presentation.tsx
@@ -1,5 +1,5 @@
 import { zodResolver } from '@hookform/resolvers/zod'
-import { FC, useEffect, useState } from 'react'
+import { FC, useEffect, useRef, useState } from 'react'
 import { useForm, useWatch } from 'react-hook-form'
 import { z } from 'zod'
 import { Room } from '../../../resources/Room/type'
@@ -8,20 +8,63 @@ import PostIt from './PostIt'
 
 import styles from './presentation.module.css'
 
-type Props = Room
+type Props = Room & {
+  onPostItMove: (props: { id: number; position: { x: number; y: number } }) => void
+}
 
-const Presentation: FC<Props> = ({ id, postIts, members }) => {
+const postItIdPrefix = 'postit_' as const
+const encodePostItIdSchema = z
+  .number()
+  .transform((id) => `${postItIdPrefix}${id}` as `${typeof postItIdPrefix}${number}`)
+type PostItId = z.infer<typeof encodePostItIdSchema>
+const decodePostItIdSchema = z
+  .custom<PostItId>()
+  .refine((postItId) => !Number.isNaN(parseInt(postItId.replace(postItIdPrefix, ''), 10)), {
+    message: 'isNan',
+  })
+  .transform((postItId) => parseInt(postItId.replace(postItIdPrefix, ''), 10))
+
+const Presentation: FC<Props> = ({ id, postIts, members, onPostItMove }) => {
+  const draggingInfo = useRef<{
+    id: number
+    startX: number
+    startY: number
+  } | null>(null)
   return (
-    <div className={styles.root}>
+    <div
+      className={styles.root}
+      onDrop={(e) => {
+        if (!draggingInfo.current) return
+
+        onPostItMove({
+          id: draggingInfo.current.id,
+          // see: https://qiita.com/numanomanu/items/e0ace008565164c17b75#step4-%E8%A4%87%E6%95%B0%E6%9E%9A%E3%81%AE%E3%82%AB%E3%83%BC%E3%83%89%E3%82%92%E7%B7%A8%E9%9B%86%E3%81%97%E3%81%9F%E3%82%8A%E3%81%86%E3%81%94%E3%81%8B%E3%81%9B%E3%82%8B%E3%82%88%E3%81%86%E3%81%AB%E3%81%99%E3%82%8B
+          position: {
+            x: e.clientX - draggingInfo.current.startX,
+            y: e.clientY - draggingInfo.current.startY,
+          },
+        })
+      }}
+      onDragOver={(e) => e.preventDefault()}
+    >
       {postIts.map((postIt) => {
         return (
           <div
+            id={encodePostItIdSchema.parse(postIt.id)}
             key={postIt.id}
             className={styles.postItPosition}
             style={{
-              top: `${postIt.position.x}px`,
-              left: `${postIt.position.y}px`,
+              top: `${postIt.position.y}px`,
+              left: `${postIt.position.x}px`,
               zIndex: postIt.position.z,
+            }}
+            draggable
+            onDragStart={(e) => {
+              draggingInfo.current = {
+                id: postIt.id,
+                startX: e.clientX - postIt.position.x,
+                startY: e.clientY - postIt.position.y,
+              }
             }}
           >
             <PostIt data={postIt} />
