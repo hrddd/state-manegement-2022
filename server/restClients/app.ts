85fc6447c8b8606d0e92ea3bf12d12231c9ad44f
dadada <nuhhunnuhhun@gmail.com>
2023-01-30 03:40:53 +0900

    move msw setup to Zodios

--- server/restClients/app.ts
+++ server/restClients/app.ts
@@ -1,8 +1,21 @@
 import { Zodios } from '@zodios/core'
 import { z } from 'zod'
+import { server } from '../../__mock__/rest/startRestMockServer'
 import { postIt } from '../../repository/postIts/type'
 
 console.log(process.env.REST_URL)
+
+// if (process.env.NODE_ENV === 'development') {
+//   if (typeof window === 'undefined') {
+//     server.listen()
+//   } else {
+//     worker.start()
+//   }
+// }
+if (typeof window === 'undefined') {
+  server.listen()
+}
+
 export const restClient = new Zodios(`${process.env.REST_URL}`, [
   {
     method: 'get',
